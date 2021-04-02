f8262328df4ebb13dab5d17e7fad56b48c191af1
gurgen <gurgenarshak@gmail.com>
2021-04-03 00:56:08 +0400

    added 301_310 exs every 3

---
+++ arrays/ex301_310.py
@@ -0,0 +1,21 @@
+import random
+def ex301(n):
+    x = []
+    k = 5 
+    for i in range(n):
+        if i % k == 0 and i>9:
+            x.append(i)
+    return x
+
+
+def ex304(arr):
+    x = []
+    n = 5
+    for a in range(arr):
+        if a % n == 2 and a > n:
+            x.append(a)
+    return x
+
+
+print(ex301(99))
+print(ex304(100))
