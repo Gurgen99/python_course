50c418c0002033f9c891966a5fdb298214ce665b
gurgen <gurgenarshak@gmail.com>
2021-04-11 20:46:05 +0400

    fix 301,304 ex

--- arrays/ex301_310.py
+++ arrays/ex301_310.py
@@ -1,21 +1,19 @@
 import random
-def ex301(n):
+def ex301(k):
     x = []
-    k = 5 
-    for i in range(n):
-        if i % k == 0 and i>9:
+    for i in range(10,100):
+        if i % k == 0:
             x.append(i)
     return x
 
 
-def ex304(arr):
+def ex304(n):
     x = []
-    n = 5
-    for a in range(arr):
-        if a % n == 2 and a > n:
+    for a in range(10,100):
+        if a % n == 2:
             x.append(a)
     return x
 
  
-print(ex301(99))
-print(ex304(100))
+print(ex301(7))
+print(ex304(8))
