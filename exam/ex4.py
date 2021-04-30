1f30c5bf06797f9890ad7764329d1f984632ca9f
gurgen <gurgenarshak@gmail.com>
2021-05-01 00:35:24 +0400

    examen

--- exam/ex4.py
+++ exam/ex4.py
@@ -1,8 +1,9 @@
-s = "hgzgazjkzfzizzzz"
+s = "bbbbazzzzaz"
 x = 0
 for i in range(len(s)):
     if s[i]=="a":
         t = i
+        break
 for i in range(t,len(s)):
     if s[i]=="z":
         x+=1
