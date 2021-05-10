94e62499eef32228a812a61ee3aedaf31b51e2f3
gurgen <gurgenarshak@gmail.com>
2021-05-10 20:33:35 +0400

    added kotorak

--- class/kotorak.py
+++ class/kotorak.py
@@ -5,14 +5,22 @@ class Kotorak():
     
     def __add__(self,other):
         tmp = Kotorak(0,0)
-        tmp.hamarich = self.hamarich + other.hamarich
-        tmp.haytarar = self.haytarar + other.haytarar
+        if self.haytarar == other.haytarar:
+            tmp.hamarich = self.hamarich + other.hamarich
+            tmp.haytarar = self.haytarar
+        else:
+            tmp.haytarar = self.haytarar * other.haytarar
+            tmp.hamarich = self.hamarich * other.haytarar + other.hamarich * self.haytarar
         return tmp
 
     def __sub__(self,other):
         tmp = Kotorak(0,0)
-        tmp.hamarich = self.hamarich - other.hamarich
-        tmp.haytarar = self.haytarar - other.haytarar
+        if self.haytarar == other.haytarar:
+            tmp.hamarich = self.hamarich - other.hamarich
+            tmp.haytarar = self.haytarar
+        else:
+            tmp.haytarar = self.haytarar * other.haytarar
+            tmp.hamarich = self.hamarich * other.haytarar - other.hamarich * self.haytarar
         return tmp
 
     def __mul__(self,other):
@@ -26,8 +34,8 @@ class Kotorak():
         tmp.hamarich = self.hamarich / other.hamarich
         tmp.haytarar = self.haytarar / other.haytarar
         return tmp
-a = Kotorak(10,20)
-b = Kotorak(5,10)
+a = Kotorak(3,4)
+b = Kotorak(2,5)
 c = a + b
 d = a - b
 e = a * b 
